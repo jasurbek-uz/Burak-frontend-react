3aa375478a9e1f165094345fa4b253ad04adaf3c
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-07-09 19:58:31 +0900

    fix: homeNavbar menu part refactor

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -6,26 +6,15 @@ export function HomeNavbar() {
 
   return (
 		<div className="home-navbar">
-			<Container sx={{ mt: "55px", height: "642px" }}>
-				<Stack
-					sx={{ height: "50px" }}
-					flexDirection={"row"}
-					justifyContent={"space-between"}
-					alignItems={"center"}
-				>
+			<Container className="navbar-container">
+				<Stack className="menu">
 					<Box>
 						<NavLink to="/">
-							<img
-								style={{ width: "125px", height: "30px" }}
-								src="/icons/burak.svg"
-							/>
+							<img className="brand-logo" src="/icons/burak.svg"/>
 						</NavLink>
 					</Box>
 					<Stack
-						flexDirection={"row"}
-						justifyContent={"space-between"}
-						minWidth={"700px"}
-						alignItems={"center"}
+						className="links"
 					>
 						<Box className={"hover-line"}>
 							<NavLink to="/" activeClassName={"underline"}>
@@ -60,10 +49,16 @@ export function HomeNavbar() {
 
 						{!authMember ? (
 							<Box>
-								<Button variant="contained" style={{background:"#3776cc", color:"#f8f8ff"}}>Login</Button>
+								<Button variant="contained" className="login-button">
+									Login
+								</Button>
 							</Box>
 						) : (
-							<img />
+							<img
+								className="user-avatar"
+								src={"/icons/default-user.svg"}
+								aria-haspopup={"true"}
+							/>
 						)}
 					</Stack>
 				</Stack>
