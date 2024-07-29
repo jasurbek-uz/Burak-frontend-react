195d73a3912ca7a1cc4cc909a00fde8f4a67ca30
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-07-29 11:33:12 +0900

    feat: develop AppRootStat and HomePageState integration

---
+++ src/lib/types/screen.ts
@@ -0,0 +1,16 @@
+import NewDishes from "../../app/screens/homePage/NewDishes";
+import { Member } from "./member";
+import { Product } from "./product";
+
+
+
+/* REACT APP STATE*/
+export interface AppRootState {
+	homePage: HomePageState;
+}
+
+export interface HomePageState {
+	popularDishes: Product[];
+	NewDishes: Product[];
+	topUsers: Member[];
+}
