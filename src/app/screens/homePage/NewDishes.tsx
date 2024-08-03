e89320c3befe2e2d2ef5cb9fbf75056c6b4c668c
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-08-03 20:09:53 +0900

    fix: mistake is fixed

--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -1,56 +1,82 @@
 import { Box, Container, Stack } from "@mui/material";
-import Card from "@mui/joy/Card";
-import { CssVarsProvider, Typography } from "@mui/joy";
-import CardOverflow from "@mui/joy/CardOverflow";
+import React from "react";
 import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import Typography from "@mui/joy/Typography";
+import { CssVarsProvider } from "@mui/joy/styles";
+import CardOverflow from "@mui/joy/CardOverflow";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import Divider from "../../components/divider";
 
 import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
-import { retrieveTopUsers } from "./selector";
+import { retrieveNewDishes, retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 import { serverApi } from "../../../lib/config";
-import { Member } from "../../../lib/types/member";
 
 /** Redux Slice & Selector */
 
-const topUserRetriever = createSelector(retrieveTopUsers, (topUsers) => ({
-	topUsers,
+const newDishesRetriever = createSelector(retrieveNewDishes, (newDishes) => ({
+	newDishes,
 }));
 
-export default function ActiveUsers() {
-	const { topUsers } = useSelector(topUserRetriever);
+export default function NewDishes() {
+	const { newDishes } = useSelector(newDishesRetriever);
 	return (
-		<div className={"active-users-frame"}>
+		<div className={"new-products-frame"}>
 			<Container>
 				<Stack className={"main"}>
-					<Box className={"category-title"}>Active Users</Box>
+					<Box className={"category-title"}>Fresh Menu</Box>
 					<Stack className={"cards-frame"}>
 						<CssVarsProvider>
-							{topUsers.length !== 0 ? (
-								topUsers.map((member: Member) => {
-									const imagePath = member.memberImage
-										? `${serverApi}/${member.memberImage}`
-										: "icons/default-user.svg";
+							{newDishes.length !== 0 ? (
+								newDishes.map((product: Product) => {
+									const imagePath = `${serverApi}/${product.productImages[0]}`;
+									const sizeVolume =
+										product.productCollection === ProductCollection.DRINK
+											? product.ProductVolume + "l"
+											: product.productSize + " size";
+
 									return (
 										<Card
-											key={member._id}
+											key={product._id}
 											variant="outlined"
 											className={"card"}
 										>
 											<CardOverflow>
+												<div className="product-sale">{sizeVolume}</div>
 												<AspectRatio ratio="1">
 													<img src={imagePath} alt="" />
 												</AspectRatio>
 											</CardOverflow>
-											<CardOverflow>
-												<Typography className={"member-nickname"}>
-													{member.memberNick}
-												</Typography>
+
+											<CardOverflow variant="soft" className="product-detail">
+												<Stack className="info">
+													<Stack flexDirection={"row"}>
+														<Typography className={"title"}>
+															{product.productName}
+														</Typography>
+														<Divider width="2" height="24" bg="#d9d9d9" />
+														<Typography className={"price"}>
+															${product.productPrice}
+														</Typography>
+													</Stack>
+													<Stack>
+														<Typography className={"views"}>
+															{product.productViews}
+															<VisibilityIcon
+																sx={{ fontSize: 20, marginLeft: "5px" }}
+															/>
+														</Typography>
+													</Stack>
+												</Stack>
 											</CardOverflow>
 										</Card>
 									);
 								})
 							) : (
-								<Box className="no-data">No Active Users!</Box>
+								<Box className="no-data">New products are not available!</Box>
 							)}
 						</CssVarsProvider>
 					</Stack>
--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -1,61 +1,105 @@
 import { Box, Container, Stack } from "@mui/material";
+import React from "react";
 import Card from "@mui/joy/Card";
-import { CssVarsProvider, Typography } from "@mui/joy";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import { CssVarsProvider } from "@mui/joy/styles";
 import CardOverflow from "@mui/joy/CardOverflow";
-import AspectRatio from "@mui/joy/AspectRatio";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
 import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
-import { retrieveTopUsers } from "./selector";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 import { serverApi } from "../../../lib/config";
-import { Member } from "../../../lib/types/member";
 
 /** Redux Slice & Selector */
 
-const topUserRetriever = createSelector(retrieveTopUsers, (topUsers) => ({
-  topUsers,
-}));
+const popularDishesRetriever = createSelector(
+	retrievePopularDishes,
+	(popularDishes) => ({ popularDishes })
+);
 
-export default function ActiveUsers() {
-  const { topUsers } = useSelector(topUserRetriever);
-  return (
-    <div className={"active-users-frame"}>
-      <Container>
-        <Stack className={"main"}>
-          <Box className={"category-title"}>Active Users</Box>
-          <Stack className={"cards-frame"}>
-            <CssVarsProvider>
-              {topUsers.length !== 0 ? (
-                topUsers.map((member: Member) => {
-                  const imagePath = member.memberImage
-                    ? `${serverApi}/${member.memberImage}`
-                    : "icons/default-user.svg";
-                  return (
-                    <Card
-                      key={member._id}
-                      variant="outlined"
-                      className={"card"}
-                    >
-                      <CardOverflow>
-                        <AspectRatio ratio="1">
-                          <img src={imagePath} alt="" />
-                        </AspectRatio>
-                      </CardOverflow>
-                      <CardOverflow>
-                        <Typography className={"member-nickname"}>
-                          {member.memberNick}
-                        </Typography>
-                      </CardOverflow>
-                    </Card>
-                  );
-                })
-              ) : (
-                <Box className="no-data">No Active Users!</Box>
-              )}
-            </CssVarsProvider>
-          </Stack>
-        </Stack>
-      </Container>
-    </div>
-  );
+export default function PopularDishes() {
+	const { popularDishes } = useSelector(popularDishesRetriever);
+	return (
+		<div className="popular-dishes-frame">
+			<Container>
+				<Stack className="popular-section">
+					<Box className="category-title">Popular Dishes</Box>
+					<Stack className="cards-frame">
+						{popularDishes.length !== 0 ? (
+							popularDishes.map((product: Product) => {
+								const imagePath = `${serverApi}/${product.productImages[0]}`;
+								return (
+									<CssVarsProvider key={product._id}>
+										<Card className="card">
+											<CardCover>
+												<img src={imagePath} alt="" />
+											</CardCover>
+											<CardCover className="card-cover" />
+											<CardContent sx={{ justifyContent: "flex-end" }}>
+												<Stack
+													flexDirection={"row"}
+													justifyContent={"space-between"}
+												>
+													<Typography
+														level="h2"
+														fontSize={"lg"}
+														textColor="#fff"
+														mb={1}
+													>
+														{product.productName}
+													</Typography>
+													<Typography
+														sx={{
+															fontweight: "md",
+															color: "neutral.100",
+															alignItems: "center",
+															display: "flex",
+														}}
+													>
+														{product.productViews}
+														<VisibilityIcon
+															sx={{
+																fontSize: 25,
+																marginLeft: "5px",
+															}}
+														/>
+													</Typography>
+												</Stack>
+											</CardContent>
+											<CardOverflow
+												sx={{
+													display: "flex",
+													gap: 1.5,
+													py: 1.5,
+													px: "var(--Card-padding)",
+													borderTop: "1px solid",
+													height: "60px",
+												}}
+											>
+												<Typography
+													startDecorator={<DescriptionOutlinedIcon />}
+													textColor="neutral.300"
+												>
+													{" "}
+													{product.productDesc}
+												</Typography>
+											</CardOverflow>
+										</Card>
+									</CssVarsProvider>
+								);
+							})
+						) : (
+							<Box className="no-data">Popular products are not available!</Box>
+						)}
+					</Stack>
+				</Stack>
+			</Container>
+		</div>
+	);
 }
