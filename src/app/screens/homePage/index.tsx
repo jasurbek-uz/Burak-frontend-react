2428a3fd95d19929d46d4f9738d61e221788c180
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-08-03 12:31:47 +0900

    feat: develop product service business logic and retrieve  data from backend server

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,32 +7,46 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
-import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
+import { setPopularDishes, setTopUsers } from "./slice";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
+import MemberService from "../../services/MemberService";
 
 // redux slice & selector
 const actionDispatch = (dispatch: Dispatch) =>({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
 
-const PopularDishesRetriever = createSelector(
-	retrievePopularDishes,
-	(popularDishes) => ({ PopularDishes})
-);
 
 export default function HomePage() {
   const { setPopularDishes } = actionDispatch(useDispatch());
-  const { PopularDishes } = useSelector(PopularDishesRetriever);
    // selector:store =>data
 
   useEffect(() => {
+    // backend server data fetch =>data
+    const product = new ProductService();
+    product.getProducts({
+      page: 1,
+      limit: 4,
+      order: "productViews",
+      productCollection: ProductCollection.DISH,
+    })
+      .then((data) => {
+      setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
     
-    // slice:Data => Store
+     const member = new MemberService();
+			member
+				.getTopUsers()
+				.then((data) => setTopUsers(data))
+				.catch((err) => console.log(err));
   }, [])
+  
+  console.log("popularDishes:", PopularDishes);
   
 	return (
 		<div className={"homepage"}>
