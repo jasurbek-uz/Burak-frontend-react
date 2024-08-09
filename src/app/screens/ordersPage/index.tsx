051de6a644938e91f5451a2b255d734e0eaf9a52
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-08-09 11:39:51 +0900

    feat: develop updateMember business logic

--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -16,6 +16,8 @@ import OrderService from "../../services/OrderService";
 import { useGlobals } from "../../hooks/useGlobals";
 import "../../../css/order.css";
 import { useHistory } from "react-router-dom";
+import { serverApi } from "../../../lib/config";
+import { MemberType } from "../../../lib/enums/member.enum";
 
 /** Redux Slice & Selector */
 const actionDispatch = (dispatch: Dispatch) => ({
@@ -93,23 +95,42 @@ export default function OrdersPage() {
 					<Box className={"order-info-box"}>
 						<Box className={"member-box"}>
 							<div className={"order-user-img"}>
-								<img src={"/img/you.webp"} className={"order-user-avatar"} />
+								<img
+									src={
+										authMember?.memberImage
+											? `${serverApi}/${authMember.memberImage}`
+											: "/icons/default-user.svg"
+									}
+									className={"order-user-avatar"}
+								/>
 								<div className={"order-user-icon-box"}>
 									<img
-										src={"/icons/user-badge.svg"}
+										src={
+											authMember?.memberType === MemberType.RESTAURANT
+												? "/icons/restaurant.svg"
+												: "/icons/user-badge.svg"
+										}
 										className={"order-user-prof-img"}
 									/>
 								</div>
 							</div>
-							<span className={"order-user-name"}>Max</span>
-							<span className={"order-user-prof"}>User</span>
+							<span className={"order-user-name"}>
+								{authMember?.memberNick}
+							</span>
+							<span className={"order-user-prof"}>
+								{authMember?.memberType}
+							</span>
 						</Box>
 						<Box className={"liner"}></Box>
 						<Box className={"order-user-address"}>
 							<div style={{ display: "flex" }}>
 								<LocationOnIcon />
 							</div>
-							<div className={"spec-address-txt"}>Do not exist</div>
+							<div className={"spec-address-txt"}>
+								{authMember?.memberAddress
+									? authMember.memberAddress
+									: "Do not exist!"}
+							</div>
 						</Box>
 					</Box>
 					<Box className={"order-info-box"} sx={{ mt: "15px" }}>
